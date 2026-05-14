c6501e245f8712de4a5af62690301e6f926b3509
Ankit Dabra <mca.ankit777@gmail.com>
2026-05-14 15:19:38 +0530

    Update files

---
+++ Server/Controller/api.js
@@ -0,0 +1,76 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+// import dotenv from 'dotenv';
+import Customer from '../Models/customermodel.js';
+// dotenv.config();
+
+mongoose.connect('mongodb://127.0.0.1:27017/Customer-Project-Database?directConnection=true')
+.then(()=>console.log('Database Connected'))
+.catch(error=>console.log('Database Not Connected'));
+
+
+const app=express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.post('/customers',async(req,res)=>{
+   try {
+     const newCustomer=new Customer({
+        customerName:req.body.customerName,
+        customerAge:req.body.customerAge,
+        customerLocation:req.body.customerLocation,
+        customerContact:req.body.customerContact
+    })
+    await newCustomer.save();
+    res.status(200).json({message:'Customer Created'});
+   } catch (error) {
+    res.status(500).json({message:'Customer Not Created'});
+   }
+});
+
+
+app.get('/customers',async(req,res)=>{
+    try {
+        const customer=await Customer.find();
+        res.json(customer);
+    } catch (error) {
+        res.status(500).json({message:'Customer Cannot be Displayed'});
+    }
+});
+
+app.put('/customers/:id',async(req,res)=>{
+    try {
+        const updateCustomer=await Customer.findByIdAndUpdate(  req.params.id,
+            {
+                 customerName:req.body.customerName,
+        customerAge:req.body.customerAge,
+        customerLocation:req.body.customerLocation,
+        customerContact:req.body.customerContact
+          
+        },
+    {new:true})
+        res.status(200).json({message:'Customer Updated',customer:updateCustomer})
+
+    } catch (error) {
+        res.status(500).json({message:'Customer Not Updated'});
+    }
+});
+
+
+app.delete('/customers/:id',async(req,res)=>{
+    try {
+        const deleteCustomer=await Customer.findByIdAndDelete(req.params.id);
+        res.status(200).json({message:'Customer Deleted',customer:deleteCustomer});
+    } catch (error) {
+        res.status(500).json({message:'Customer Not Deleted'});
+    }
+});
+
+app.listen(8686,()=>{
+    console.log('Server running at http://127.0.0.1:8686');
+});
+
