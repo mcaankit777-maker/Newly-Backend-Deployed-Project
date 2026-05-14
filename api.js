2158bfc8fb686c0b2a64cf58ae9d17a5c9c1c850
mcaankit777-maker <mca.ankit777@gmail.com>
2026-05-14 17:27:42 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
+
 import Customer from 'customermodel.js';
+
+import dotenv from 'dotenv';
 
 dotenv.config();
 
