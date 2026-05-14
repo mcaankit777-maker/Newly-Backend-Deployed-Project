fd231c4126fe3f9cddf6e12b15e9bc148fa25d44
mcaankit777-maker <mca.ankit777@gmail.com>
2026-05-14 17:48:58 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
-import Customer from './Models/customermodel.js';
 
+import Customer from './Models/customermodel.js';
+import dotenv from 'dotenv';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
